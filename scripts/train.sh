5bf05eae690df8d3825dfee1e9ca7ed9fc7d5e00
fradlin <ilyafr97@gmail.com>
2024-06-17 13:19:24 +0200

    Small config adjustments

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export OMP_NUM_THREADS=12  # speeds up MinkowskiEngine
-export CUDA_LAUNCH_BLOCKING=1
 export HYDRA_FULL_ERROR=1
+# export CUDA_LAUNCH_BLOCKING=1
 # export NCCL_DEBUG=INFO
 
 # TRAIN
